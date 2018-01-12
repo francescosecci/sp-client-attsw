56d2bd824818dea7c1a0a5b5af672f23248b5ed0
francescosecci <francescosecci@172.25.51.97>
2018-01-12 14:40:32 +0100

    created RestServiceClient and RestServiceClientTest

---
+++ client/src/test/java/com/pufose/client/RestServiceClient.java
@@ -0,0 +1,5 @@
+package com.pufose.client;
+
+public class RestServiceClient {
+
+}
---
+++ client/src/test/java/com/pufose/client/RestServiceClientTest.java
@@ -0,0 +1,26 @@
+package com.pufose.client;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+public class RestServiceClientTest {
+
+	private RestServiceClient fixture;
+	private WireMockServer mockedServer;
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
