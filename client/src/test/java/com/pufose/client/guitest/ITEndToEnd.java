b876552380e4865a6139edeb0d91d99243cf45eb
Alex Foglia <alex.f1793@gmail.com>
2018-02-08 23:57:26 +0100

    add an end to end test case excluded from build

---
+++ client/src/test/java/com/pufose/client/guitest/ITEndToEnd.java
@@ -0,0 +1,73 @@
+package com.pufose.client.guitest;
+
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.pufose.client.gui.GUI;
+
+@RunWith(JUnit4.class)
+public class ITEndToEnd  {
+	private FrameFixture window;
+	private GUI frame;
+	@Before
+	public void runGui() {
+		frame = GuiActionRunner.execute(()-> GUI.createGui(false));
+		window=new FrameFixture(frame.getFrame());
+		window.show();
+	}
+	@After
+	public void tearDownGui() {
+		window.cleanUp();
+	}
+	@Test
+	public void testCanRetrieveAllGrids() {
+		window.button("btnCreateConn").click();
+		window.comboBox("actionsCombo").selectItem(0);
+		window.button("btnPerform").click();
+		window.label("lblOutput").requireText(GUI.OPERATION_OK);
+		
+	}
+	@Test
+	public void testCanRequestOneGrid() {
+		
+		window.button("btnCreateConn").click();
+		window.comboBox("actionsCombo").selectItem(0);
+		window.button("btnPerform").click();
+		window.comboBox("actionsCombo").selectItem(1);
+		window.button("btnPerform").click();
+		window.label("lblOutput").requireText(GUI.OPERATION_OK);
+		
+	}
+	@Test
+	public void testCanRequestShortestPath() {
+		window.button("btnCreateConn").click();
+		window.comboBox("actionsCombo").selectItem(0);
+		window.button("btnPerform").click();
+		window.comboBox("actionsCombo").selectItem(1);
+		window.button("btnPerform").click();
+		window.textBox("sourceField").setText("0_0");
+		window.textBox("sinkField").setText("0_1");
+		window.comboBox("actionsCombo").selectItem(2);
+		window.button("btnPerform").click();
+		window.label("lblOutput").requireText(GUI.OPERATION_OK);
+	}
+	@Test
+	public void testCanRequestShortestPathErrored() {
+		window.button("btnCreateConn").click();
+		window.comboBox("actionsCombo").selectItem(0);
+		window.button("btnPerform").click();
+		window.comboBox("actionsCombo").selectItem(1);
+		window.button("btnPerform").click();
+		window.textBox("sourceField").setText("0_0");
+		window.textBox("sinkField").setText("0_3");
+		window.comboBox("actionsCombo").selectItem(2);
+		window.button("btnPerform").click();
+		window.label("lblOutput").requireText(GUI.SERVER_ERROR);
+	}
+
+}
